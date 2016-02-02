c2f2bf8ac61ae1b534bb8098dad5ea33343fafaa
Robert McNeill <rjmcneill@users.noreply.github.com>
2016-02-02 13:35:29 +0000

    Manual xAxis labels implemented

--- src/assets/js/util/domain/domain.js
+++ src/assets/js/util/domain/domain.js
@@ -2,10 +2,14 @@ import centerOnDate from './centerOnDate';
 import filterDataInDateRange from './filterDataInDateRange';
 import moveToLatest from './moveToLatest';
 import trackingLatestData from './trackingLatestData';
+import roundTimestampDown from './roundTimestampDown';
+import roundTimestampUp from './roundTimestampUp';
 
 export default {
     centerOnDate: centerOnDate,
     filterDataInDateRange: filterDataInDateRange,
     moveToLatest: moveToLatest,
-    trackingLatestData: trackingLatestData
+    trackingLatestData: trackingLatestData,
+    roundTimestampDown: roundTimestampDown,
+    roundTimestampUp: roundTimestampUp
 };
