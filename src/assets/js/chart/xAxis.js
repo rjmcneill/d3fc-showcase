5b799d884ecd61014dd651bed2a8f1541f1688a1
Robert McNeill <rjmcneill@users.noreply.github.com>
2016-03-29 10:52:40 +0100

    Updated xAxis labelling to match new UX designs

--- src/assets/js/chart/xAxis.js
+++ src/assets/js/chart/xAxis.js
@@ -11,10 +11,25 @@ export default function() {
 
     function preventTicksMoreFrequentThanPeriod(period) {
         var scaleTickSeconds = (xScale.ticks()[1] - xScale.ticks()[0]) / 1000;
+
         if (scaleTickSeconds < period.seconds) {
             xAxis.ticks(period.d3TimeInterval.unit, period.d3TimeInterval.value);
         } else {
             xAxis.ticks(6);
+        }
+    }
+
+    function adaptTickFormatting() {
+        var scaleTickSeconds = (xScale.ticks()[1] - xScale.ticks()[0]) / 1000;
+
+        if (scaleTickSeconds <= 1800) {
+            xAxis.tickFormat(d3.time.format('%H:%M, %d %b'));
+        } else if (scaleTickSeconds <= 21600) {
+            xAxis.tickFormat(d3.time.format('%I %p, %a %d %b'));
+        } else if (scaleTickSeconds <= 608400) {
+            xAxis.tickFormat(d3.time.format('%a %d, %b %Y'));
+        } else if (scaleTickSeconds <= 2592000) {
+            xAxis.tickFormat(d3.time.format('%B, %Y'));
         }
     }
 
@@ -25,7 +40,28 @@ export default function() {
         xScale.discontinuityProvider(model.discontinuityProvider);
 
         preventTicksMoreFrequentThanPeriod(model.period);
+        adaptTickFormatting();
+
         selection.call(xAxis);
+
+        selection.selectAll('text')
+            .call(function(text) {
+                text.each(function() {
+                    var _this = d3.select(this);
+                    var split = _this.text().split(',');
+                    _this.text(null);
+                    _this.append('tspan')
+                        .attr('class', 'axis-label-main')
+                        .attr('dy', '0.6em')
+                        .attr('x', 0)
+                        .text(split[0]);
+                    _this.append('tspan')
+                        .attr('class', 'axis-label-secondary')
+                        .attr('dy', '1em')
+                        .attr('x', 0)
+                        .text(split[1]);
+                });
+            });
     }
 
     xAxisChart.dimensionChanged = function(container) {
